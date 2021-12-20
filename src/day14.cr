17d7f5259f90de110721349b96165c58f37d876d
Joshua Paine <joshua@letterblock.com>
2021-12-20 11:28:10 -0500

    day 14, part 2

--- src/day14.cr
+++ src/day14.cr
@@ -1,6 +1,5 @@
 INPUT = File.read("input/day14")
 initial,rules_lines = INPUT.split("\n\n",2)
-polymer = initial.chars
 rules = rules_lines.split("\n").reduce(Hash({Char,Char},Char).new) do |memo,line|
   x,d = line.split(" -> ",2)
   a,b = x.chars
@@ -8,21 +7,35 @@ rules = rules_lines.split("\n").reduce(Hash({Char,Char},Char).new) do |memo,line
   memo
 end
 
-10.times do 
-  p2 = Array(Char).new(polymer.size * 2)
-  (0...(polymer.size - 1)).each do |i|
-    p2 << polymer[i]
-    p2 << rules[{polymer[i],polymer[i+1]}]
-  end
-  p2 << polymer.last(1)[0]
-  polymer = p2
+chars = initial.chars
+last_char = chars[chars.size-1]
+pair_counts = Hash({Char,Char},Int64).new(0,rules.size)
+(0...(chars.size - 1)).each do |i|
+  pair_counts[{chars[i],chars[i+1]}] += 1
 end
 
-counts = polymer.reduce(Hash(Char,Int32).new {|h,k| h[k] = 0}) do |memo,char|
-  memo[char] += 1
-  memo
-end.values
+def polymerize(pair_counts,rules)
+  pair_counts.reduce(Hash({Char,Char},Int64).new(0,rules.size)) do |memo,(pair,count)|
+    y = rules[pair]
+    memo[{pair[0],y}] += count
+    memo[{y,pair[1]}] += count
+    memo
+  end
+end
 
-min,max = counts.minmax
+def count_elements(pair_counts,last_char)
+  counts = Hash(Char,Int64).new(0,10)
+  counts[last_char] += 1
+  counts = pair_counts.reduce(counts) do |memo,(pair,count)|
+    memo[pair[0]] += count
+    memo
+  end
+end
 
-puts "Most common - least common = #{max-min}"
+10.times { pair_counts = polymerize(pair_counts,rules) }
+min,max = count_elements(pair_counts,last_char).values.minmax
+puts "After 10: Most common - least common = #{max-min}"
+
+30.times { pair_counts = polymerize(pair_counts,rules) }
+min,max = count_elements(pair_counts,last_char).values.minmax
+puts "After 40: Most common - least common = #{max-min}"
