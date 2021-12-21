c26cfefd11a17cc451f3cf240c6ae81f3b4dd990
Joshua Paine <joshua@letterblock.com>
2021-12-20 20:11:55 -0500

    day 15 part 1

--- src/day15.cr
+++ src/day15.cr
@@ -1,36 +1,55 @@
 INPUT = File.read("input/day15")
+# INPUT = "1163751742
+# 1381373672
+# 2136511328
+# 3694931569
+# 7463417111
+# 1319128137
+# 1359912421
+# 3125421639
+# 1293138521
+# 2311944581"
 
-grid = INPUT.split("\n").map {|line| line.chars.map &.to_i8 }
-start = {0.to_i16,0.to_i16}
-goal = {(grid.size - 1).to_i16,(grid[0].size - 1).to_i16}
-
-def step(grid,goal,pos,risk=0,path=Set({Int16,Int16}).new,best=Int32::MAX)
-  a,b = pos
-  risk += grid[a][b]
-  path << pos
-  return {false,risk} if risk >= best
-  return {true,risk} if pos == goal
-  options = Array({Int16,Int16}).new(4)
-  options << {a-1,b} unless a == 0 || path.includes?({a-1,b})
-  options << {a,b+1} unless b+1 == grid[a].size || path.includes?({a,b+1})
-  options << {a+1,b} unless a+1 == grid.size || path.includes?({a+1,b})
-  options << {a,b-1} unless b == 0 || path.includes?({a,b-1})
-  return {false,risk} if options.empty?
-  new_best = best
-  options.each do |opt|
-    success,score = step(grid,goal,opt,risk,path.dup,new_best)
-    new_best = score if success
-    # puts path.inspect if success
-  end
-  new_best < best ? {true,new_best} : {false,best}
+def print_grid(grid)
+  grid.each {|line| puts line.join(',') }
 end
 
-success,risk_score = step(grid,goal,start,-1*grid[start[0]][start[1]].to_i32)
+grid = INPUT.split("\n").map {|line| line.chars.map &.to_i8 }
+max = {(grid.size - 1),(grid[0].size - 1)}
+start = {0,0}
+goal = max
+risk_map = Array.new(grid.size) { Array(Int32).new(grid[0].size,Int32::MAX) }
+risk_map[start.first][start.last] = 0
+touches = Array.new(grid.size) { Array(Int32).new(grid[0].size,0) }
 
-puts "Lowest possible risk score: #{risk_score}"
+steps = Array({Int32,Int32}).new(400)
+steps << start
+best = Int32::MAX
+count = 0
+while(step = steps.shift?)
+  count += 1
+  a,b = step
+  risk = risk_map[a][b]
+  next if (best < Int32::MAX) && (risk + (goal.first - a).abs + (goal.last - b).abs > best)
+  touches[a][b] += 1
+  next best = {risk,best}.min if {a,b} == goal
+  ((a==0 ? 0 : -1)..(a==max.first ? 0 : 1)).each do |da|
+    ((b==0 ? 0 : -1)..(b==max.last ? 0 : 1)).each do |db|
+      next if da.abs == db.abs
+      na = a+da
+      nb = b+db
+      nrisk = risk + grid[na][nb]
+      next if nrisk >= risk_map[na][nb]
+      risk_map[na][nb] = nrisk
+      steps << {na,nb} unless steps.includes?({na,nb})
+    end
+  end
+  puts steps.size
+end
 
-# This approach works for a 10x10 board, but it takes too long for the 100x100 
-# board. I thought I was being clever by stopping each explorer if its total 
-# risk exceeded the best-so-far risk, but I can do better by keeping track of 
-# the best-so-far risk to reach each square and prune most branches much faster.
-# I think that approach will also benefit from a breadth-first approach.
+puts "Considered #{count}"
+print_grid touches
+puts "\n"
+print_grid risk_map
+
+puts "Lowest possible risk score: #{best}"
