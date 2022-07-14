7c8b1e5894d6f5074f7e62f829b2b18ac7905ae0
Joshua Paine <joshua@letterblock.com>
2022-07-14 11:40:32 -0400

    make better use of elixir built-ins

--- src/day6.exs
+++ src/day6.exs
@@ -1,14 +1,14 @@
 # Part 2 only; ~20 min
 days = 256 # change to 80 for Part 1
-File.read!("input/day6") |> String.split(",") |> Enum.map(fn s -> Integer.parse(s) |> elem(0) end)
-|> Enum.reduce(%{}, fn age, pop -> Map.update(pop, age, 1, fn v -> v + 1 end) end)
+File.read!("input/day6") |> String.split(",")
+|> Enum.frequencies_by(fn s -> Integer.parse(s) |> elem(0) end)
 |> then(fn pop ->
   Enum.reduce(1..days, pop, fn _, pop ->
-    Enum.flat_map(pop, fn
-      {age, count} when age == 0 -> [{6, count},{8, count}]
-      {age, count} -> [{age - 1, count}]
+    Enum.map(pop, fn
+      {age, count} when age == 0 -> %{6 => count, 8 => count}
+      {age, count} -> %{(age - 1) => count}
     end)
-    |> Enum.reduce(%{}, fn {age, count}, pop -> Map.update(pop, age, count, fn v -> v + count end) end)
+    |> Enum.reduce(fn a, b -> Map.merge(a, b, fn _, v, w -> v + w end) end)
   end)
 end)
 |> Map.values |> Enum.sum |> IO.puts
