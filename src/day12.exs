77a1fd650d0416a448c745c0f4701b35b0c90699
Joshua Paine <joshua@letterblock.com>
2022-07-19 10:06:07 -0400

    day 12, first solid loss for elixir vs crystal

--- src/day12.exs
+++ src/day12.exs
@@ -1,29 +1,36 @@
 import Enum
 defmodule Caves do
-  @lowcase ~r/^[a-z]+$/
   def parse(lines) do
-    lines |> reduce(%{}, fn line, connections ->
+    lines |> reduce(%{}, fn line, caves ->
       Regex.run(~r/(\w+)-(\w+)/, line)
       |> tl
-      |> map(&(if String.match?(&1, @lowcase), do: String.to_atom(&1), else: &1))
+      |> map(&atomize_lcase/1)
       |> then(fn [left, right] ->
-        connections
-        |> Map.update(left, [right], &([right | &1]))
-        |> Map.update(right, [left], &([left | &1]))
+        caves |> add(left, right) |> add(right, left)
       end)
     end)
   end
-  def traverse({[:end|connections],caves}), do: 1 + traverse({connections,caves})
-  def traverse({dead_end,_caves}) when dead_end in [[], nil], do: 0
-  def traverse({[next|connections],caves}) when is_atom(next) do
-    traverse(Map.pop(caves,next,nil)) + traverse({connections,caves})
+  defp atomize_lcase(str), do: if(String.match?(str, ~r/^[a-z]+$/), do: String.to_atom(str), else: str)
+  defp add(caves, _a, :start), do: caves # Don't make connections back to :start
+  defp add(caves, a, b), do: Map.update(caves, a, [b], &([b | &1]))
+  
+  def traverse(_, [], _, _), do: 0
+  def traverse(caves, [:end | connections], visited, ar), do: 1 + traverse(caves, connections, visited, ar)
+  def traverse(caves, [room | connections], visited, allow_revisit) do
+    {returning, deeper} = Map.get_and_update(visited, room, &({(&1||false),true}))
+    revisit = is_atom(room) and returning
+    case {revisit, allow_revisit} do
+      {true, false} -> 0
+      _ -> traverse(caves, Map.fetch!(caves, room), deeper, allow_revisit and not revisit)
+    end + traverse(caves, connections, visited, allow_revisit)
   end
-  def traverse({[next|connections],caves}) do
-    traverse({Map.fetch!(caves,next), caves}) + traverse({connections,caves})
-  end
-  def traverse(caves), do: traverse(Map.pop!(caves,:start))
+  def traverse(caves, allow_revisit), do: traverse(caves, Map.fetch!(caves,:start), %{}, allow_revisit)
+  def traverse(caves), do: traverse(caves, false)
 end
 
 File.stream!("input/day12")
 |> Caves.parse
-|> Caves.traverse |> IO.puts
+|> then(fn caves ->
+  Caves.traverse(caves) |> IO.puts
+  Caves.traverse(caves, true) |> IO.puts
+end)
