ef5f198d5354395368caffa48eb0bec82b3c56d0
Joshua Paine <joshua@letterblock.com>
2022-07-21 20:52:06 -0400

    day 15 clearer, can do part 2 in < 45s

--- src/day15.exs
+++ src/day15.exs
@@ -3,21 +3,37 @@ defmodule Navigate do
   defstruct grid: "", width: 0, height: 0, goal: {0,0}
   @moves [{1,0},{0,1},{-1,0},{0,-1}]
   def load_file(path) do
-    nav = StringIO.open("", fn pid ->
-      {width,height} = File.stream!(path)
-      |> reduce({0,0}, fn line, {w,h} ->
-        line = String.trim(line)
-        bytes = line
-          |> :binary.bin_to_list
-          |> Enum.map(&(&1 - 48))
-          |> :binary.list_to_bin
-        IO.write pid, bytes
-        {max([w,byte_size(line)]), h + 1}
+    {grid,{width,height}} = File.stream!(path)
+    |> reduce({%{},{0,0}}, fn line, {grid,{w,h}} ->
+      line = String.trim(line)
+      grid = line
+      |> :binary.bin_to_list
+      |> map(&(&1 - 48))
+      |> with_index
+      |> reduce(grid, fn {v, x}, grid ->
+        Map.put(grid,{x,h},v)
       end)
-      {_, grid} = StringIO.contents pid
-      %Navigate{ grid: grid, width: width, height: height }
-    end) |> elem(1)
-    %{nav | goal: {nav.width-1, nav.height-1}}
+      {grid,{max([w,byte_size(line)]), h + 1}}
+    end)
+    %Navigate{ grid: grid, width: width, height: height, goal: {width-1, height-1} }
+  end
+
+  def expand(nav, ex, ey) do
+    grid = 0..ex
+    |> flat_map(fn sx -> 0..ey |> map(&({sx, &1})) end)
+    |> map(fn {sx,sy} ->
+      Map.new(nav.grid, fn {{x,y}, v} ->
+        v = case rem(v + sx + sy, 9) do
+          0 -> 9
+          v -> v
+        end
+        {{x + nav.width * sx, y + nav.height * sy}, v}
+      end)
+    end)
+    |> reduce(&Map.merge/2)
+    width = nav.width * (ex + 1)
+    height = nav.height * (ey + 1)
+    %Navigate{ grid: grid, width: width, height: height, goal: {width-1, height-1} }
   end
 
   defp risk_at(nav, {x,y}) do
@@ -51,30 +67,36 @@ defmodule Navigate do
     end
   end
 
-  defp path(nav, coords \\ {0,0}, risk \\ 0, bests \\ %{}) do
-    @moves |> reduce(bests, fn move, bests ->
-      with {:ok, coords} <- valid_move(nav, coords, move),
-           risk <- (risk_at(nav, coords) + risk),
-           :ok <- plausible_path(nav, bests, coords, risk),
-           {:ok, bests} <- new_best(bests, coords, risk) do
-        case coords == nav.goal do
-          true -> bests
-          false -> path(nav, coords, risk, bests)
-        end
+  defp path(nav, bests \\ %{{0,0}=>0}, to_visit \\ [{0,0}], to_visit_set \\ MapSet.new)
+  defp path(nav, bests, [], _), do: Map.get(bests, nav.goal)
+  defp path(nav, bests, [coords|to_visit], to_visit_set) do
+    to_visit_set = MapSet.delete(to_visit_set, coords)
+    {more_steps, {bests, to_visit_set}} = @moves |> flat_map_reduce({bests, to_visit_set}, fn move, {bests, to_visit_set} ->
+      with {:ok, ncoords} <- valid_move(nav, coords, move),
+           risk <- (Map.get(nav.grid, ncoords) + Map.get(bests, coords)),
+           :ok <- plausible_path(nav, bests, ncoords, risk), # This saves maybe 1.5s out of ~45s
+           {:ok, bests} <- new_best(bests, ncoords, risk) do
+        {
+          if(ncoords == nav.goal, do: [], else: [ncoords]),
+          {bests, MapSet.put(to_visit_set, ncoords)}
+        }
       else
-        _ -> bests
+        _ -> {[],{bests,to_visit_set}}
       end
     end)
+    path(nav, bests, to_visit ++ more_steps, to_visit_set)
   end
 
   def min_risk(nav) do
     start = Time.utc_now
-    risk = path(nav) |> Map.get(nav.goal)
+    risk = path(nav)
     {risk, Time.diff(Time.utc_now, start, :millisecond)}
   end
 end
 
 Navigate.load_file("input/day15")
+|> IO.inspect
+|> Navigate.expand(4,4)
 |> IO.inspect
 |> Navigate.min_risk
 |> IO.inspect
